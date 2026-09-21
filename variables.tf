5e65c2f10fa56b6d4c24cd2a0e92572e560f1e60
Neha <93898137+nehaafridi@users.noreply.github.com>
2026-09-21 17:19:16 +0530

    Add Terraform variables

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  description = "AWS region where the infrastructure will be created"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
