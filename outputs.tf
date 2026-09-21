951407a2cc7b29039d6a2246ac9d80a5744c3e06
Neha <93898137+nehaafridi@users.noreply.github.com>
2026-09-21 17:25:58 +0530

    Add Terraform outputs

---
+++ outputs.tf
@@ -0,0 +1,14 @@
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = aws_vpc.main.id
+}
+
+output "vpc_cidr_block" {
+  description = "CIDR block of the VPC"
+  value       = aws_vpc.main.cidr_block
+}
+
+output "aws_region" {
+  description = "AWS region used for the infrastructure"
+  value       = var.aws_region
+}
